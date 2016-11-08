f4f20295568280d41f7cacecd14e123892169b15
MTmotong <dzymt6627@163.com>
2016-11-08 11:18:05 +0800

    123

--- src/com/db/Dao.java
+++ src/com/db/Dao.java
@@ -27,7 +27,7 @@ public class Dao
   { 
     String driver = "com.mysql.jdbc.Driver";
     String username = "root";
-    String password = "root";
+    String password = "199587";
     String dbUrl = String.format("jdbc:mysql://%s:%s/%s", new Object[] {
       "localhost", "3306", "staples" });    
     
